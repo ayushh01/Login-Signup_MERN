fd3ff57aa584228e536ee651b3c74dec36d045e4
Kartik Agarwal <32024148+kartikag1@users.noreply.github.com>
2020-07-24 19:31:30 +0530

    add timestamps to monitor well.... :)
    avoid redundant variables as well....

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
 
-const UserSchema = new Schema({
+const UserSchema = new mongoose.Schema({
     name:{
         type:String,
         required:true
@@ -17,7 +16,8 @@ const UserSchema = new Schema({
     date:{
         type:Date,
         default:Date.now
-    }
+    },
+    { timestamps: true }
 });
 
 const User = mongoose.model('User',UserSchema);
